c67bedbc784e73b9d755722a2f57e68e2d417f1a
Magnus Nordlander <magnus@nordlander.se>
2015-08-13 00:25:11 +0200

    Added composer.json, cleaned out crap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,10 +1,3 @@
 <?php
-require_once __DIR__.'/../vendor/classloader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-use Symfony\Component\ClassLoader\UniversalClassLoader;
 
-$loader = new UniversalClassLoader();
-$loader->registerNamespaces(array(
-    'Messy'          => array(__DIR__.'/../src', __DIR__)
-));
-
-$loader->register();
+require_once __DIR__.'/../vendor/autoload.php';
